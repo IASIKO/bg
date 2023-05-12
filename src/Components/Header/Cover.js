d9aea4876946ac5409349f15558baea0944b93f4
IASIKO <iaseshviligi@gmail.com>
2023-05-12 12:24:47 +0100

    add

--- src/Components/Header/Cover.js
+++ src/Components/Header/Cover.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from './Cover.module.css'
+import logo from '../../Assets/logo.png'
 
 const Cover = () => {
   return (
     <section className={styles.cover}>
       <div className={styles.backgroundOverlay}>
-
       </div>
+          <img src={logo} alt="logo"/>
     </section>
   );
 };
