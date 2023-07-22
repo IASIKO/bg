69aa3c5fd3b51a52cb57b70f26db08e863f8aebf
IASIKO <iaseshviligi@gmail.com>
2023-07-22 21:42:57 +0400

    add

--- src/Components/Pages/RootLayout.js
+++ src/Components/Pages/RootLayout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Header/Header'
 import { Outlet } from 'react-router-dom'
+import Footer from '../Footer/Footer'
 
 const RootLayout = () => {
   return (
     <>
         <Header/>
         <Outlet/>
+        <Footer/>
     </>
   )
 }
