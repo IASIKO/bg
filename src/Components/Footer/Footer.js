de45f8a3c198d8faf0e4e144934c20e07c5a7efb
IASIKO <iaseshviligi@gmail.com>
2023-07-23 20:03:26 +0400

    add

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,15 +1,16 @@
-import React from 'react'
-
-import styles from './Footer.module.css'
+import React from "react";
+import { MdLocationOn } from "react-icons/md";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <section className={styles.footer}>
-        <div className={styles.footerBackgroundOverlay}>
-        </div>
-        
+      <div className={styles.footerBackgroundOverlay}></div>
+      <span>
+        <MdLocationOn />
+      </span>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
