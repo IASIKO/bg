7e9ca7fc2044c91595fe3e65c44aa9e35d4f3cde
IASIKO <iaseshviligi@gmail.com>
2023-07-24 21:44:55 +0400

    add

--- src/Components/Pages/home/BG.js
+++ src/Components/Pages/home/BG.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import styles from './BG.module.css'
+import React from "react";
+import styles from "./BG.module.css";
+import { Link } from "react-router-dom";
 
-const BG = () => {
+const BG = (props) => {
   return (
-    <li className={styles.bg}>
-        
-    </li>
-  )
-}
+    <>
+      {props.bgData.map((item) => (
+        <li className={styles.bg}>
+          <div className={styles.imgDiv}>
+            <Link>
+              <img src={item.img} alt={item.title} />
+            </Link>
+          </div>
+          <div>
+            <Link>
+              <h2>{item.title}</h2>
+            </Link>
+            <span>{item.price}</span>
+          </div>
+        </li>
+      ))}
+    </>
+  );
+};
 
-export default BG
+export default BG;
--- src/Components/Pages/home/BGsList.js
+++ src/Components/Pages/home/BGsList.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import styles from './BGsList.module.css'
-import BG from './BG'
+import React from "react";
+import styles from "./BGsList.module.css";
+import BG from "./BG";
 
+const DUMMY_BGs = [
+  {
+    id: "1",
+    img: "https://corners.ge/wp-content/uploads/2023/06/Rebellion-Unplugged-Sniper-Elite-The-Board-Game-247x247.jpg",
+    title: "Sniper Elite",
+    price: "400.00 ₾",
+  },
+  {
+    id: "1",
+    img: "https://corners.ge/wp-content/uploads/2023/06/Rebellion-Unplugged-Sniper-Elite-The-Board-Game-247x247.jpg",
+    title: "Sniper Elite",
+    price: "400.00 ₾",
+  },
+  {
+    id: "1",
+    img: "https://corners.ge/wp-content/uploads/2023/06/Rebellion-Unplugged-Sniper-Elite-The-Board-Game-247x247.jpg",
+    title: "Sniper Elite",
+    price: "400.00 ₾",
+  },
+];
 
 const BGsList = () => {
   return (
     <>
-        <h1 className={styles.lastAddedTitle}>Last added</h1>
-        <ul className='bgs'>
-          <BG/>
-        </ul>
+      <h1 className={styles.lastAddedTitle}>Last added</h1>
+      <ul className={styles.bgs}>
+        <BG bgData={DUMMY_BGs} />
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default BGsList
+export default BGsList;
