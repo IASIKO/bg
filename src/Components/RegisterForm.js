bff0dd403ac8b3c6b5384c2f8a83b025a08151c1
IASIKO <iaseshviligi@gmail.com>
2023-07-26 23:19:51 +0400

    add

--- src/Components/RegisterForm.js
+++ src/Components/RegisterForm.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./RegisterForm.module.css";
 
 const RegisterForm = () => {
+  const [enteredFirstName, setEnteredFirstName] = useState();
+
+  const firstNameRef = useRef("");
+
+  const firstNameInputChangeHandler = () => {
+    setEnteredFirstName(firstNameRef.current.value);
+  };
+  console.log(enteredFirstName);
+
   return (
     <>
       <form className={styles.form}>
         <h1>Create a new user</h1>
         <p>
           <label htmlFor="firstName">First Name</label>
-          <input id="firstName" type="firstName" name="firstName" required />
+          <input
+            id="firstName"
+            type="firstName"
+            name="firstName"
+            ref={firstNameRef}
+            onChange={firstNameInputChangeHandler}
+            required
+          />
         </p>
         <p>
           <label htmlFor="lastName">Last Name</label>
