ec73800180e5f712c603601b46bb046b52dd690a
IASIKO <iaseshviligi@gmail.com>
2023-08-11 17:09:44 +0400

    add

--- src/application/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-export const ProtectedRoute = ({ hasAccess, children }) => {
-  if (!hasAccess) {
-    return <Navigate to="/" replace />;
-  }
-  return children;
-};
