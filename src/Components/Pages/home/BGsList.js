a6c16adb1470ebb5710dd7f851e7c4d7a7023baf
IASIKO <iaseshviligi@gmail.com>
2023-08-14 16:29:30 +0400

    add

--- src/Components/Pages/home/BGsList.js
+++ src/Components/Pages/home/BGsList.js
@@ -1,11 +1,9 @@
 import React from "react";
 import styles from "./BGsList.module.css";
-import BG from "./BG";
 import { useSelector } from "react-redux";
 import BGProductsList from "../BoardGamesPage/BGProductsList";
 
 const BGsList = () => {
-  const productsData = useSelector((state) => state.user.product.productsData);
 
   return (
     <>
