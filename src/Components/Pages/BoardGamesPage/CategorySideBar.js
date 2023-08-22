46c06f866efba2f934165fa5b2d8b089a3853a68
IASIKO <iaseshviligi@gmail.com>
2023-08-22 15:07:02 +0400

    add category sidebar styles

--- src/Components/Pages/BoardGamesPage/CategorySideBar.js
+++ src/Components/Pages/BoardGamesPage/CategorySideBar.js
@@ -1,24 +1,26 @@
 import React from "react";
 import styles from "./CategorySideBar.module.css";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const CategorySideBar = () => {
   const categories = useSelector((state) => state.user.product.categories);
   return (
     <section className={styles.sidebarWrapper}>
       <h2>Categories</h2>
-      <ul className={styles.categoriesList}>
         {categories.map((item) => {
           return (
-            <li key={item._id}>
-              <Link to={`/boardgames/categories/${item.name}`}>
-                {item.name}
-              </Link>
-            </li>
+            <NavLink
+              key={item._id}
+              to={`/boardgames/categories/${item.name}`}
+              className={({ isActive }) =>
+                isActive ? styles.active : undefined
+              }
+            >
+              {item.name}
+            </NavLink>
           );
         })}
-      </ul>
     </section>
   );
 };
