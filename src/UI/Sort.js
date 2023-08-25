e0feedb6e6574cfc99e1bed08fcf297fb80e94f6
IASIKO <iaseshviligi@gmail.com>
2023-08-25 20:33:23 +0400

    update styles

--- src/UI/Sort.js
+++ src/UI/Sort.js
@@ -1,11 +1,14 @@
 import React from "react";
+import styles from "./Sort.module.css";
 
 const Sort = ({ handleChange }) => {
   return (
     <>
-      <label>Sort By</label>
-      <select onChange={(e) => handleChange(e.target.value)}>
-        <option value="none">Default</option>
+      <select
+        onChange={(e) => handleChange(e.target.value)}
+        className={styles.sortSelect}
+      >
+        <option value="none">Default sorting</option>
         <option value="ascending">Alphabetically, A-Z</option>
         <option value="descending">Alphabetically, Z-A</option>
         <option value="high">Low to high (Price)</option>
