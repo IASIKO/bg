14f348b4932aa22122bb89be44832e839d51c798
IASIKO <iaseshviligi@gmail.com>
2023-08-25 15:12:44 +0400

    add home category content

--- src/Components/Pages/home/Categories.js
+++ src/Components/Pages/home/Categories.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import styles from './Categories.module.css'
+import React from "react";
+import styles from "./Categories.module.css";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const Categories = () => {
-  return (
-    <div>
-        <h1 className={styles.categoriesTitle}>Categories</h1>
-        
-    </div>
-  )
-}
+  const categories = useSelector((state) => state.user.product.categories);
+  const navigate = useNavigate();
 
-export default Categories
+  const onCategoryClickHandler = (categoryName) => {
+    navigate(`/boardgames/categories/${categoryName}`);
+  };
+
+  return (
+    <>
+      <h1 className={styles.categoriesTitle}>Categories</h1>
+      <div className={styles.homeCategoriesContainer}>
+        {categories.map((item) => {
+          return (
+            <span onClick={() => onCategoryClickHandler(item.name)}>
+              {item.name}
+            </span>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Categories;
