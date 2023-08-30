ae703513bd8cc7af8f3f42bb10c6ae4375f631da
IASIKO <iaseshviligi@gmail.com>
2023-08-30 20:40:44 +0400

    corection structure HTML

--- src/Components/Pages/Contact/ContactContent.js
+++ src/Components/Pages/Contact/ContactContent.js
@@ -6,26 +6,27 @@ const ContactContent = () => {
   return (
     <section className={styles.contactWrapper}>
       <div className={styles.contentContainer}>
-        <h1>About us</h1>
-        <p>
-          Board Games Corner is the largest store of board games in Georgia,
-          where more than 300 cognitive, entertaining and interesting board
-          games are collected. In addition to the store, there is board games
-          club too, where you can play various board games.
-        </p>
+        <div className={styles.about}>
+          <h1>About us</h1>
+          <p>
+            Board Games Corner is the largest store of board games in Georgia,
+            where more than 300 cognitive, entertaining and interesting board
+            games are collected. In addition to the store, there is board games
+            club too, where you can play various board games.
+          </p>
+        </div>
         <div className={styles.timeTitle}>
           <strong>მაღაზიის სამუშაო საათები:</strong>
           <br />
           <strong>ყოველდღე (12:00 – 22:00)</strong>
         </div>
 
-        <div>
+        <div className={styles.infoWrapper}>
+          
           <div className={styles.iconInfoContainer}>
-            <div>
-              <i>
-                <FaPhone />
-              </i>
-            </div>
+            <i>
+              <FaPhone />
+            </i>
           </div>
           <div className={styles.textInfoContainer}>
             <h3>Phone</h3>
@@ -35,33 +36,30 @@ const ContactContent = () => {
           </div>
 
           <div className={styles.iconInfoContainer}>
-            <div>
-              <i>
-                <FaRegEnvelopeOpen />
-              </i>
-            </div>
-            <div className={styles.textInfoContainer}>
-              <h3>E-mail</h3>
-              <p>
-                <span>info.boardgames@gmail.com</span>
-              </p>
-            </div>
+            <i>
+              <FaRegEnvelopeOpen />
+            </i>
+          </div>
+          <div className={styles.textInfoContainer}>
+            <h3>E-mail</h3>
+            <p>
+              <span>info.boardgames@gmail.com</span>
+            </p>
           </div>
         </div>
 
         <div className={styles.iconInfoContainer}>
-          <div>
-            <i>
-              <FaMapMarkerAlt />
-            </i>
-          </div>
-          <div className={styles.textInfoContainer}>
-            <h3>Address</h3>
-            <p>
-              <span>Tbilisi, 99 Tina Iosebidze St</span>
-            </p>
-          </div>
+          <i>
+            <FaMapMarkerAlt />
+          </i>
         </div>
+        <div className={styles.textInfoContainer}>
+          <h3>Address</h3>
+          <p>
+            <span>Tbilisi, 99 Tina Iosebidze St</span>
+          </p>
+        </div>
+
       </div>
 
       <div className={styles.contentContainer}>
