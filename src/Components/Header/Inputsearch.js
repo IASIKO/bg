3fca1cbaf2e51cb9c530f78b95bdfa5f27e75537
IASIKO <iaseshviligi@gmail.com>
2023-09-08 15:41:39 +0400

    add

--- src/Components/Header/Inputsearch.js
+++ src/Components/Header/Inputsearch.js
@@ -31,33 +31,31 @@ const Inputsearch = () => {
   const isDropdownVisible = searchValue && searchResults.length > 0;
 
   return (
-    <>
-      <div className={styles.dropdawn}>
-        <input
-          className={styles.searchInput}
-          type="search"
-          placeholder="Search here..."
-          value={searchValue}
-          onChange={(e) => {
-            setSearchValue(e.target.value);
-          }}
-        />
-        <div className={styles.dropdownContent}>
-          {isDropdownVisible && (
-            <>
-              {searchResults.map((product) => (
-                <div key={product._id} value={product.name} className={styles.dropdownItem}>
-                  {`${product.name} $${product.price}`}
-                </div>
-              ))}
-            </>
-          )}
-        </div>
-        <button onClick={() => navigate(`/boardgames/${searchValue}`)}>
-          Search
-        </button>
+    <div className={styles.dropdawn}>
+      <input
+        className={styles.searchInput}
+        type="search"
+        placeholder="Search here..."
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+        }}
+      />
+      <div className={styles.dropdownContent}>
+        {isDropdownVisible && (
+          <>
+            {searchResults.map((product) => (
+              <div key={product._id} className={styles.dropdownItem}>
+                {`${product.name} $${product.price}`}
+              </div>
+            ))}
+          </>
+        )}
       </div>
-    </>
+      <button onClick={() => navigate(`/boardgames/${searchValue}`)}>
+        Search
+      </button>
+    </div>
   );
 };
 
