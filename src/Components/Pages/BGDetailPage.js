1245bdc2d7d4797f7b080b567a706a3e92e6fcda
IASIKO <iaseshviligi@gmail.com>
2023-09-12 15:51:20 +0400

    add

--- src/Components/Pages/BGDetailPage.js
+++ src/Components/Pages/BGDetailPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useParams } from "react-router-dom";
 import { fetchSingleProductById } from "../../redux/slices/productSlice";
+import Body from "../../UI/Body";
 
 const BGDetailPage = () => {
   const singleProduct = useSelector(
@@ -17,7 +18,11 @@ const BGDetailPage = () => {
     dispatch(fetchSingleProductById({ id: state.id }));
   }, [dispatch, state.id]);
 
-  return <div>{params.name}</div>;
+  return (
+    <>
+      <Body>{params.name}</Body>
+    </>
+  );
 };
 
 export default BGDetailPage;
