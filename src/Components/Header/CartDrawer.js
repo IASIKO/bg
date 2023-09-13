3cbd7459f248752486a0771aecfde3139980b579
IASIKO <iaseshviligi@gmail.com>
2023-09-13 20:37:32 +0400

    save close icon

--- src/Components/Header/CartDrawer.js
+++ src/Components/Header/CartDrawer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./CartDrawer.module.css";
+import { GrClose } from "react-icons/gr";
 
 const CartDrawer = ({ isOpen, isClose }) => {
   return (
     <div className={`${styles.cart} ${isOpen ? styles.open : ""}`}>
       <div className={styles.cartContent}></div>
-      <button className={styles.closeButton} onClick={isClose}>
-        Close
-      </button>
+      <div className={styles.close} onClick={isClose}>
+        <GrClose />
+      </div>
     </div>
   );
 };
