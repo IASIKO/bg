e06f91bf8e65af6487a637fbadf79a0c4470f484
IASIKO <iaseshviligi@gmail.com>
2023-09-14 19:41:21 +0400

    update last added

--- src/Components/Pages/home/BGsList.js
+++ src/Components/Pages/home/BGsList.js
@@ -1,30 +1,20 @@
 import React, { useEffect } from "react";
 import styles from "./BGsList.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { isUserAdmin } from "../../../application/utilis";
-import { useNavigate } from "react-router-dom";
 import {
   fetchProducts,
-  setSelectedProduct,
 } from "../../../redux/slices/productSlice";
 import Loader from "../../../UI/Loader";
 
 const BGsList = () => {
   const productsData = useSelector((state) => state.user.product.productsData);
-  const userInfo = useSelector((state) => state.user.user.userData);
   const isLoading = useSelector((state) => state.user.product.loading);
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(fetchProducts());
-  }, []);
-
-  const onEditHandler = (product) => {
-    dispatch(setSelectedProduct(product));
-    navigate(`/boardgames/edit/:${product._id}`);
-  };
+  }, [dispatch]);
 
   return (
     <>
@@ -40,9 +30,6 @@ const BGsList = () => {
                   <img src={product.image} alt={product.name} />
                   <h2>{product.name}</h2>
                   <p>{product.price} ₾</p>
-                  {isUserAdmin(userInfo) && (
-                    <button onClick={() => onEditHandler(product)}>Edit</button>
-                  )}
                 </li>
               );
             })}
